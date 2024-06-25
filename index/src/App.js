c55137642581355ea67c0e1d068aaae44e775902
unknown <krithikroshan113@gmail.com>
2024-06-25 22:18:37 +0530

    react first project

---
+++ index/src/App.js
@@ -0,0 +1,37 @@
+import Premium from "./premium/Premium";
+
+function App() {
+  const plan = ["FREE", "PLUS", "PRO"];
+  const price = [0, 9, 49];
+  const benefits = [
+    "Single User",
+    "50GB Storage",
+    "Unlimited Public Projects",
+    "Community Access",
+    "Unlimited Private Projects",
+    "Dedicated Phone Support",
+    "Free Subdomain",
+    "Monthly Status Reports",
+  ];
+  return (
+    <div className="App">
+      <Premium key={price[0]}
+        plan={plan[0]}
+        price={price[0]}
+        benefits={benefits}
+      />
+      <Premium
+        plan={plan[1]}
+        price={price[1]}
+        benefits={benefits}
+      />
+      <Premium
+        plan={plan[2]}
+        price={price[2]}
+        benefits={benefits}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ index/src/premium/Premium.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+import { TiTick } from "react-icons/ti";
+import { RxCross2 } from "react-icons/rx";
+
+const Premium = ({ plan, price, benefits }) => {
+  return (
+    <div>
+      <header>
+        <p>{plan}</p>
+        <h3>${price}/month</h3>
+        <hr />
+      </header>
+      <section>
+        {benefits.map((val, index) => {
+          if (price === 0 && plan === "FREE" && index < 4) {
+            return (
+              <p key={index}>
+                <TiTick />
+                {val}
+              </p>
+            );
+          } else if (price === 9 && plan === 'PLUS' && index <7 ) {
+          return (
+            <p key={index}>
+              <TiTick />
+              {val}
+            </p>
+          );
+          } else if (price === 49 && plan === 'PRO' && index < 8) {
+             return (
+               <p key={index}>
+                 <TiTick />
+                 {val}
+               </p>
+             );
+          } else {
+             return (
+               <p className="RxCross2" key={index}>
+                 <RxCross2 />
+                 {val}
+               </p>
+             );
+          }
+        })}
+      </section>
+      <footer>
+        <button>BUTTON</button>
+      </footer>
+    </div>
+  );
+};
+
+export default Premium;
